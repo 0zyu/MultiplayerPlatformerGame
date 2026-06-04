4b566f9862bfdcba374a810426148d3e13d92875
Ben McIlroy <benmcilroy07@gmail.com>
2026-06-04 21:00:17 +0100

    moved base texture loading into game.cpp, just shows a blank window atm but its expected

--- Game.cpp
+++ Game.cpp
@@ -43,6 +43,50 @@ bool Game::init()
         { 4375.f, 575.f }
     };
 
+    SDL_Surface* surfaceKnight = IMG_Load("assets/knightSprite1.png");
+    SDL_Surface* surfacePlatform = IMG_Load("assets/platform.png");
+    SDL_Surface* surfaceBackground = IMG_Load("assets/Clouds.png");
+    SDL_Surface* surfaceGround = IMG_Load("assets/ground.png");
+    SDL_Surface* surfaceEnemy = IMG_Load("assets/enemy1.png");
+    SDL_Surface* surfaceRolling = IMG_Load("assets/rollRight1.png");
+    SDL_Surface* surfaceCoin = IMG_Load("assets/coin1.png");
+    SDL_Surface* surfaceFlag = IMG_Load("assets/betterflag1.png");
+
+    if (!surfaceKnight || !surfacePlatform || !surfaceBackground || !surfaceGround ||
+        !surfaceEnemy || !surfaceRolling || !surfaceCoin || !surfaceFlag)
+    {
+        return false;
+    }
+
+    flagTexture = SDL_CreateTextureFromSurface(renderer, surfaceFlag);
+    coinTexture = SDL_CreateTextureFromSurface(renderer, surfaceCoin);
+    rollingTexture = SDL_CreateTextureFromSurface(renderer, surfaceRolling);
+    backgroundTexture = SDL_CreateTextureFromSurface(renderer, surfaceBackground);
+    enemyTexture = SDL_CreateTextureFromSurface(renderer, surfaceEnemy);
+    knightTexture = SDL_CreateTextureFromSurface(renderer, surfaceKnight);
+    platformTexture = SDL_CreateTextureFromSurface(renderer, surfacePlatform);
+    groundTexture = SDL_CreateTextureFromSurface(renderer, surfaceGround);
+
+    SDL_SetTextureScaleMode(flagTexture, SDL_SCALEMODE_NEAREST);
+    SDL_SetTextureScaleMode(coinTexture, SDL_SCALEMODE_NEAREST);
+    SDL_SetTextureScaleMode(rollingTexture, SDL_SCALEMODE_NEAREST);
+    SDL_SetTextureScaleMode(enemyTexture, SDL_SCALEMODE_NEAREST);
+    SDL_SetTextureScaleMode(knightTexture, SDL_SCALEMODE_NEAREST);
+    SDL_SetTextureScaleMode(platformTexture, SDL_SCALEMODE_NEAREST);
+    SDL_SetTextureScaleMode(groundTexture, SDL_SCALEMODE_NEAREST);
+
+    SDL_DestroySurface(surfaceFlag);
+    SDL_DestroySurface(surfaceCoin);
+    SDL_DestroySurface(surfaceRolling);
+    SDL_DestroySurface(surfaceBackground);
+    SDL_DestroySurface(surfaceEnemy);
+    SDL_DestroySurface(surfaceKnight);
+    SDL_DestroySurface(surfacePlatform);
+    SDL_DestroySurface(surfaceGround);
+
+
+
+
     return true;
 }
 
@@ -57,6 +101,15 @@ void Game::run()
 
 void Game::clean()
 {
+    SDL_DestroyTexture(flagTexture);
+    SDL_DestroyTexture(coinTexture);
+    SDL_DestroyTexture(rollingTexture);
+    SDL_DestroyTexture(backgroundTexture);
+    SDL_DestroyTexture(enemyTexture);
+    SDL_DestroyTexture(knightTexture);
+    SDL_DestroyTexture(platformTexture);
+    SDL_DestroyTexture(groundTexture);
+
     TTF_CloseFont(font);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
@@ -67,8 +120,6 @@ void Game::clean()
 
 void Game::handleEvents()
 {
-    SDL_Event event;
-
     while (SDL_PollEvent(&event))
     {
         if (event.type == SDL_EVENT_QUIT)
--- Game.h
+++ Game.h
@@ -1,14 +1,19 @@
 #pragma once
 
 #include <SDL3/SDL.h>
+#include <iostream>
+#include <SDL3_image/SDL_image.h>
 #include <SDL3_ttf/SDL_ttf.h>
-#include <vector>
 
+#include <string>
+#include <vector>
+#include "Collision.h"
 #include "Player.h"
 #include "Enemy.h"
-#include "Flag.h"
 #include "Coin.h"
+
 #include "Platform.h"
+#include "Flag.h"
 
 class Game
 {
@@ -25,6 +30,17 @@ private:
     SDL_Window* window = nullptr;
     SDL_Renderer* renderer = nullptr;
     TTF_Font* font = nullptr;
+    SDL_Texture* backgroundTexture = nullptr;
+    SDL_Texture* groundTexture = nullptr;
+    SDL_Texture* platformTexture = nullptr;
+    SDL_Texture* knightTexture = nullptr;
+    SDL_Texture* enemyTexture = nullptr;
+    SDL_Texture* rollingTexture = nullptr;
+    SDL_Texture* coinTexture = nullptr;
+    SDL_Texture* flagTexture = nullptr;
+
+
+    SDL_Event event;
 
     Player player;
     Enemy enemy;
@@ -35,6 +51,14 @@ private:
 
     bool gameLoop = true;
     bool gameStarted = false;
+
+	//Camera variables
+    float cameraX = 0.0f;
+    
+    //Gravity variables
+    float gravity = 1800.0f; //gravity strength and how much it pulls down
+    float jumpStrength = -700.0f; //how high the player will jump
+    float speed = 500.0f;
 
     float screenWidth = 1500;
     float screenHeight = 800;
